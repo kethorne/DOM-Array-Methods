7b74fd05ba8bb774a1402a3574d1c86dcefa436a
Karin Thorne <kethorne17@gmail.com>
2022-05-04 14:02:36 -0400

    Started js file functions

---
+++ index.js
@@ -0,0 +1,36 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+
+let data = [];
+
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch a random user from the API and add their wealth from the data
+// returned from the API call
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    };
+
+    console.log(newUser);
+    addData(newUser);
+}
+
+
+//Add new obj to a data array
+function addData(obj) {
+    data.push(obj);
+}
